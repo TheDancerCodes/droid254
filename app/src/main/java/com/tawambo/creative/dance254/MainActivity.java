10081abb404d4cc5eb1d4875f8eeb5e5dd72d7de
Roger Taracha <rojtaracha@gmail.com>
2018-05-15 17:30:28 +0300

    Initial Commit
    [+] Project Setup

---
+++ app/src/main/java/com/tawambo/creative/dance254/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tawambo.creative.dance254;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
