e4e6504f4943f841151fec253fa75aff7c016231
Roger Taracha <rojtaracha@gmail.com>
2018-05-15 17:56:40 +0300

    [CHORE] Add Crashlytics to Project.
    [+] App Crash Reporting.

--- app/src/main/java/com/tawambo/creative/dance254/MainActivity.java
+++ app/src/main/java/com/tawambo/creative/dance254/MainActivity.java
@@ -2,12 +2,17 @@ package com.tawambo.creative.dance254;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Initialize Crashlytics Kit
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
     }
 }
