2f7489387d347ccb220c5bb5a6a4f45794ab25e2
Roger Taracha <rojtaracha@gmail.com>
2020-01-09 16:55:13 +0300

    Firebase App Distribution Implementation: {+} Firebase SDK setup by adding google-services.json file
    {+} Added Firebase App Distribution dependencies.
    
    {+} Updated Gradle versions and migrated to AndroidX.

--- app/src/main/java/com/tawambo/creative/dance254/MainActivity.java
+++ app/src/main/java/com/tawambo/creative/dance254/MainActivity.java
@@ -1,8 +1,8 @@
 package com.tawambo.creative.dance254;
 
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
